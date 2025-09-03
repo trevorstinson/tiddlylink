db70c76e8d1752497b765c9c664cfc01d55bd29c
Trevor Stinson <trevor@trevorstinson.com>
2025-09-02 19:21:54 -0500

    Add Cleaned Link input and minimal styles

--- src/components/LinkCleaner/LinkCleaner.tsx
+++ src/components/LinkCleaner/LinkCleaner.tsx
@@ -6,13 +6,21 @@ interface LinkCleanerProps {}
 
 const LinkCleaner: FC<LinkCleanerProps> = () => {
   const [urlInput, setUrlInput] = React.useState('');
+  const [urlOutput, setUrlOutput] = React.useState('');
 
   return (
     <div className={styles.LinkCleaner}>
       <TextInput
+        id="url-input"
         label="Original Link"
         value={urlInput}
         setValue={setUrlInput}
+      />
+      <TextInput
+        id="url-output"
+        label="Cleaned Link"
+        value={urlOutput}
+        setValue={setUrlOutput}
       />
     </div>
   );
--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -2,20 +2,17 @@ import { Dispatch, FC, SetStateAction } from 'react';
 import styles from './TextInput.module.css';
 
 interface TextInputProps {
+  id: string;
   label: string;
   value: string;
   setValue: Dispatch<SetStateAction<string>>;
 }
 
-const TextInput: FC<TextInputProps> = ({
-  label = 'Input',
-  value,
-  setValue,
-}) => {
+const TextInput: FC<TextInputProps> = ({ id, label, value, setValue }) => {
   const fieldId = `${label.toLocaleLowerCase()}-field`;
 
   return (
-    <div className={styles.TextInput}>
+    <div id={id} className={styles.TextInput}>
       <label htmlFor={fieldId}>{label}:</label>
       <input
         id={fieldId}
