f61ab3859b653ea78eccfe247257a37946b12030
Trevor Stinson <trevor@trevorstinson.com>
2025-09-19 15:49:48 -0500

    Change project name to Tiddlylink

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-## tidylink
+## tiddlylink
 
-A simple tool for removing tracking parameters from URLs. 
+A simple tool for removing tracking parameters from URLs.
 
 This absolutely doesn't need to be React, but it is because it exists mostly to play around with Parcel and React project configuration.
--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import LinkCleaner from './components/LinkCleaner/LinkCleaner';
 export function App() {
   return (
     <>
-      <h1>TidyLink</h1>
+      <h1>Tiddlylink</h1>
       <p>Paste your link and a cleaned copy will be provided.</p>
       <p>This does exactly one thing right now: remove all query parameters.</p>
       <LinkCleaner />
