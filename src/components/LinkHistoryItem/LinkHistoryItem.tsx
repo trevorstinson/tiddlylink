95eb60aec9169edcbbb77f3197c90147a1aa96eb
Trevor Stinson <trevor@trevorstinson.com>
2025-10-06 20:00:02 -0500

    Rename copy button symbols to allow for other buttons

--- src/components/LinkHistoryItem/LinkHistoryItem.tsx
+++ src/components/LinkHistoryItem/LinkHistoryItem.tsx
@@ -8,23 +8,23 @@ interface LinkHistoryItemProps {
 }
 
 const LinkHistoryItem: FC<LinkHistoryItemProps> = ({ children }) => {
-  const [clicked, setClicked] = React.useState(false);
+  const [copyClicked, setCopyClicked] = React.useState(false);
 
-  const handleClick = async (): Promise<void> => {
-    setClicked(true);
+  const handleCopy = async (): Promise<void> => {
+    setCopyClicked(true);
 
     await window.navigator.clipboard.writeText(children);
 
     window.setTimeout(() => {
-      setClicked(false);
+      setCopyClicked(false);
     }, 1500);
   };
 
   return (
     <div className={styles.LinkHistoryItem}>
       <pre>{children}</pre>
-      <Button handleClick={handleClick}>
-        {!clicked ? <Copy size={24} /> : <CopyCheck size={24} />}
+      <Button handleClick={handleCopy}>
+        {!copyClicked ? <Copy size={24} /> : <CopyCheck size={24} />}
       </Button>
     </div>
   );
